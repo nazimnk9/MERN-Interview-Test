9df498b832a6ebf1d64571317d10d7e4271acdef
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-09-16 07:41:47 +0600

    multi routes handle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Layout from "./pages/Layout"
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Templates from "./components/Templates";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children : [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/projects',
+        element: <Projects />
+      },
+      {
+        path: '/templates',
+        element: <Templates />
+      },
+    ]
   }
 ])
 
