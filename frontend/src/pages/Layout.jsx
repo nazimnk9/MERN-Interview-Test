49db83f77976c2721f01d0243e304f37158e02b4
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-09-16 07:31:09 +0600

    Sidebar design

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { BsFolder, BsGrid1X2 } from "react-icons/bs";
 const Layout = () => {
+  const { pathname } = useLocation();
   const [show, setShow] = useState(false);
   return (
-    <div className="bg-[#18191b] max-h-screen w-full">
+    <div className="bg-[#18191b] min-h-screen w-full">
       <div className="bg-[#252627] shadow-md fixed left-0 top-0 w-full z-20">
         <div className="w-[93%] m-auto py-3">
           <div className="flex justify-between items-center">
@@ -37,8 +40,12 @@ const Layout = () => {
                     alt="profile"
                   />
                   <div className="flex justify-center flex-col items-start">
-                    <span className="text-[#e0dddd] font-bold text-md">Md. Nazim Ahmed</span>
-                    <span className="text-[#c4c0c0] font-bold text-md">nazimnk9@gmail.com</span>
+                    <span className="text-[#e0dddd] font-bold text-md">
+                      Md. Nazim Ahmed
+                    </span>
+                    <span className="text-[#c4c0c0] font-bold text-md">
+                      nazimnk9@gmail.com
+                    </span>
                   </div>
                 </div>
                 <ul className="text-[#e0dddd] font-semibold">
@@ -56,6 +63,66 @@ const Layout = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="w-full flex mt-16">
+        <div className="sidebar w-[300px] p-5 h-[calc(100vh-70px)] fixed">
+          <div className="px-2 py-2 flex justify-start gap-5 items-center mb-3">
+            <img
+              className="w-[40px] h-[40px] rounded-full"
+              src="https://app.creativeitinstitute.com/images/student/md-nazim-ahmed_1691320973.png"
+              alt="image"
+            />
+            <div className="flex justify-center flex-col items-start">
+              <span className="text-[#e0dddd] font-bold text-md">
+                Md. Nazim Ahmed
+              </span>
+              <span className="text-[#c4c0c0] text-sm">Free</span>
+            </div>
+          </div>
+          <ul className="px-4 flex flex-col gap-2">
+            <li>
+              <Link to="/"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/" ? "bg-[#4f50a8]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <FaHome />
+                </span>
+                <span className="font-medium">Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/projects" ? "bg-[#4f50a8]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <BsFolder />
+                </span>
+                <span className="font-medium">Projects</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/templates"
+                className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${
+                  pathname === "/templates" ? "bg-[#4f50a8]" : ""
+                } rounded-[4px]`}
+              >
+                <span className="text-xl">
+                  <BsGrid1X2 />
+                </span>
+                <span className="font-medium">Templates</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="ml-[300px] w-[calc(100%-300px)]">
+            <div className="py-4 pr-4">
+                <Outlet />
+            </div>
         </div>
       </div>
     </div>
