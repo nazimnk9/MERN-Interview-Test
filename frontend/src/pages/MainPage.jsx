a08b5c820b2e9c365a74f8352a51675356180da9
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2024-09-17 08:32:04 +0600

    background color element design and remove backgroud setup

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { BsGrid1X2, BsFillImageFill, BsFolder } from "react-icons/bs";
 import { FaCloudUploadAlt, FaShapes } from "react-icons/fa";
@@ -15,6 +15,8 @@ import CreateComponent from "../components/CreateComponent";
 const MainPage = () => {
   const [state, setState] = useState("");
   const [current_component, setCurrentComponent] = useState("");
+  const [color, setColor] = useState("");
+  const [image, setImage] = useState("");
   const [show, setShow] = useState({
     status: true,
     name: "",
@@ -28,39 +30,59 @@ const MainPage = () => {
     });
   };
 
-  const moveElement = ()=>{
-    console.log('move');
-  }
+  const moveElement = () => {
+    console.log("move");
+  };
 
-  const resizeElement = ()=>{
-    console.log('resizeElement');
-  }
+  const resizeElement = () => {
+    console.log("resizeElement");
+  };
 
-  const rotateElement = ()=>{
-    console.log('rotateElement');
-  }
+  const rotateElement = () => {
+    console.log("rotateElement");
+  };
 
   const [components, setComponents] = useState([
     {
-        name: "main_frame",
-        type: "rectangle",
-        id: Math.floor((Math.random() * 100) + 1),
-        width: 650,
-        height: 450,
-        z_index: 1,
-        color: "#fff",
-        image: "",
-        setCurrentComponent: (a)=>setCurrentComponent(a)
-    }
+      name: "main_frame",
+      type: "rectangle",
+      id: Math.floor(Math.random() * 100 + 1),
+      width: 650,
+      height: 450,
+      z_index: 1,
+      color: "#fff",
+      image: "",
+      setCurrentComponent: (a) => setCurrentComponent(a),
+    },
   ]);
 
-  const removeComponent = ()=>{
+  const removeComponent = () => {
     console.log("removeComponent");
-    
-  }
+  };
+  const remove_background = () => {
+    //console.log("remove_background");
+    const comp = components.find((c) => c.id === current_component.id);
+    const temp = components.filter((c) => c.id !== current_component.id);
+    comp.image = "";
+    setImage("");
+    setComponents([...temp, comp]);
+  };
 
-  console.log(current_component);
-  
+  //console.log(current_component);
+  useEffect(() => {
+    //console.log(image)
+    if (current_component) {
+      const index = components.findIndex((c) => c.id === current_component.id);
+      const temp = components.filter((c) => c.id !== current_component.id);
+      if (current_component.name === "main_frame" && image) {
+        //console.log(image);
+        components[index].image = image || current_component.image;
+      }
+      //setComponents([...temp, components[index]])
+      components[index].color = color || current_component.color;
+      setComponents([...temp, components[index]]);
+    }
+  }, [color, image]);
 
   return (
     <div className="min-w-screen h-screen bg-black">
@@ -194,34 +216,85 @@ const MainPage = () => {
               <div className="h-[88vh] overflow-x-auto flex justify-start items-start scrollbar-hide">
                 <div className="w-full grid grid-cols-2 gap-2">
                   {[1, 2, 3, 4].map((img, i) => (
-                    <Link
+                    <div
+                      onClick={() =>
+                        setImage("http://localhost:5173/project.png")
+                      }
                       key={i}
                       className="w-full h-[90px] overflow-hidden rounded-sm cursor-pointer"
                     >
                       <img
                         className="w-full h-full object-fill"
                         src={`http://localhost:5173/project.png`}
-                        alt=""
+                        alt="image"
                       />
-                    </Link>
+                    </div>
                   ))}
                 </div>
               </div>
             )}
           </div>
           <div className="w-full flex h-full">
-            <div className={`flex justify-center relative items-center h-full ${!current_component ? 'w-full' : 'w-[calc(100%-250px)] overflow-hidden'}`}>
+            <div
+              className={`flex justify-center relative items-center h-full ${
+                !current_component
+                  ? "w-full"
+                  : "w-[calc(100%-250px)] overflow-hidden"
+              }`}
+            >
               <div className="m-w-[650px] m-h-[480px] flex justify-center items-center overflow-hidden">
-                <div id="main_design" className="w-auto h-auto overflow-hidden relative">
-                  {
-                    components.map((c,i)=><CreateComponent key={i} info={c} current_component={current_component} removeComponent={removeComponent} />)
-                  }
+                <div
+                  id="main_design"
+                  className="w-auto h-auto overflow-hidden relative"
+                >
+                  {components.map((c, i) => (
+                    <CreateComponent
+                      key={i}
+                      info={c}
+                      current_component={current_component}
+                      removeComponent={removeComponent}
+                    />
+                  ))}
                 </div>
               </div>
             </div>
-            {
-              current_component && <div className="h-full w-[250px] text-gray-300 bg-[#252627] px-3 py-2">Nazim</div>
-            }
+            {current_component && (
+              <div className="h-full w-[250px] text-gray-300 bg-[#252627] px-3 py-2">
+                <div className="flex gap-3 flex-col items-start h-full px-3 justify-start">
+                  <div className="flex gap-4 justify-start items-start">
+                    <span>Color: </span>
+                    <label
+                      className="w-[30px] h-[30px] cursor-pointer rounded-sm"
+                      style={{
+                        background: `${
+                          current_component.color &&
+                          current_component.color !== "#fff"
+                            ? current_component.color
+                            : "gray"
+                        }`,
+                      }}
+                      htmlFor="color"
+                    ></label>
+                    <input
+                      onChange={(e) => setColor(e.target.value)}
+                      type="color"
+                      className="invisible"
+                      id="color"
+                    />
+                  </div>
+                  {current_component.name === "main_frame" && image && (
+                    <div>
+                      <button
+                        className="p-[6px] bg-white text-black rounded-sm"
+                        onClick={remove_background}
+                      >
+                        Remove Background
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
